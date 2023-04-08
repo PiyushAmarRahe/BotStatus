dd86205f1e9a009874dde8e4c14c32b7e6a099ac
Piyush Malviya <115225926+CoderXPiyush@users.noreply.github.com>
2023-04-08 18:37:06 +0530

    Update Status.py

--- Status.py
+++ Status.py
@@ -10,17 +10,17 @@ from pyrogram.errors import FloodWait
 
 app = Client(
     name = "piyush",
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"],
-    session_string = os.environ["SESSION_STRING"]
+    api_id = int(os.environ["13925915"]),
+    api_hash = os.environ["f32643eb85c09b70557aec76a5a7c91a"],
+    session_string = os.environ["BQCNlmmfkfLY5H_6_Ggey9RimfD-y3XYAdUkl1KIQtid3SB6N9WPG4X0cqVmzSQE1O1vl2t0-VH9fKLYSvujVH9EW19SbZSw3WR9eZ9UGTpEK-40q_NazDaI95w6l6LPAtbsU_vd6sLOu49KcdbkEMrOtmeedsjbovM4ua74yTu5pSbv5tZfa4F8LJzHpaj6y9Qxmg8U6xnT4uRLtbtM-ZyhpKGrK_7kuvRnI-0YgTn8a2TWrLHCje7_1fJLNu14gXTmjncPxd8gfkzymz3EdZfvFFyHFdU_6Fl93cpAf4ah4vYOvhIo3EVkAKG7OZVlxCBxfIcjLUDtubH5rBvFVKe1AAAAAWLayyUA"]
 )
 
-TIME_ZONE = os.environ["TIME_ZONE"]
-BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
-CHANNEL_ID = int(os.environ["CHANNEL_ID"]) #CHANNEL_ID is for group/channel where checker will update the status.
-MESSAGE_ID = int(os.environ["MESSAGE_ID"])
-BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
-GRP_ID = os.environ.get("GRP_ID") #GRP_ID is for logs group where checker will send warnings of offline bots.
+TIME_ZONE = os.environ["Asia/Kolkata"]
+BOT_LIST = [i.strip() for i in os.environ.get("PiyushXManagerBot oppiyushmusicbot Jessica_Chat_Bot").split(' ')]
+CHANNEL_ID = int(os.environ["-1001814898068"]) #CHANNEL_ID is for group/channel where checker will update the status.
+MESSAGE_ID = int(os.environ["105"])
+BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("5900873171 5953473317").split(' ')]
+GRP_ID = os.environ.get("-1001767109485") #GRP_ID is for logs group where checker will send warnings of offline bots.
 
 async def main_piyushchecker():
     async with app:
